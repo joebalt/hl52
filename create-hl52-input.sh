a34712fd01f276aebc739beb44369cc1fff5c320
Joe Baltimore <joe.baltimore@sas.com>
2016-09-16 08:48:07 -0400

    variable-ize everything...

--- create-hl52-input.sh
+++ create-hl52-input.sh
@@ -1,21 +1,48 @@
 #!/usr/bin/env bash
 
-# steps to take getWatchListDetail.csv, dl'ed from fidelity.com
+# steps to take getWatchlistDetail.csv, dl'ed from fidelity.com
 #    to hl52.input4 - suitable for input to hl52.py
 
-cat getWatchlistDetail.csv | cut -d ',' -f 2 >hl52.input
+infile="getWatchlistDetail.csv"
+input1="hl52.input1"
+input2="hl52.input2"
+input3="hl52.input3"
+input4="hl52.input4"
 
-cat hl52.input | head -42 >hl52.input2
+echo "Cleaning up previous run artifacts..."
+rm -f $input1 $input2 $input3 $input4
 
-cat hl52.input2 | tail -38 >hl52.input3
+headerLines=4
+summaryLines=14
 
-sed -i "s/\"//g" hl52.input3
+# get total number of lines w/o headers included
+infileLineCount=$(cat $infile | wc -l)
+linesMinusHeaders=$(( $infileLineCount - $headerLines ))
 
-cat hl52.input3 | sort >hl52.input4
+echo "Total lines in $infile: $infileLineCount"
+echo "Lines minus headers: $linesMinusHeaders"
 
-echo "hl52.input4 created!"
+echo "Getting the 2nd field from the file..."
+cat $infile | cut -d ',' -f 2 >$input1
 
-cat -n hl52.input4
+echo "Trimming out header lines..."
+cat $input1 | tail -$linesMinusHeaders >$input2
+
+# get total number of lines w/o the ending summary lines
+totalLines=$(cat $input2 | wc -l)
+linesMinusSummary=$(( $totalLines - $summaryLines ))
+echo "Lines minus summary: $linesMinusSummary"
+echo "Trimming out summary lines..."
+cat $input2 | head -$linesMinusSummary >$input3
+
+echo "Removing double quotes..."
+sed -i "s/\"//g" $input3
+
+echo "Sorting results..."
+cat $input3 | sort >$input4
+
+echo "$input4 created!"
+
+cat -n $input4
 
 exit 0
-
